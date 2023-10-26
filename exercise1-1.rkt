89412a1f55f904526a15231f65cad769d9765037
Kshitij Saraogi <kshitij.saraogi@gmail.com>
2023-10-26 12:32:26 +0200

    feat: first exercise

---
+++ exercise1-1.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+10
+; 10
+(+ 5 3 4)
+; 12
+(- 9 1)
+; 8
+(/ 6 2)
+; 3
+(+ (* 2 4) (- 4 6))
+; 6
+(define a 3)
+; Nothing is printed for the define special form.
+(define b (+ a 1))
+; Nothing is printed for the define special form.
+(+ a b (* a b))
+; 19
+(= a b)
+; #f
+(if (and (> b a) (< b (* a b)))
+    b
+    a)
+; 4
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+; 16
+(+ 2 (if (> b a) b a))
+; 6
+(* (cond ((> a b) a) 
+         ((< a b) b)
+         (else -1)) 
+    (+ a 1))
+; 16
